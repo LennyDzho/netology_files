5cc08e10d5abfb0bd0fb379de3fd8a623f267ddd
LennyDzho <nastratob@gmail.com>
2024-08-05 17:51:41 +0300

    Добавил комментарий

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ print(cook_dict)
 
 '''
 Задание 2
-Функция, которая создает словарь на основе файлике лежит 
-в папке в for_organization в файлике organizations_dishes
+Функция лежит в папке в for_organization
+ в файлике organizations_dishes
 
 Не знаю, как сделать без цикличного импорта, и при этом повторно не считывать файлик recipes.txt,
 поэтому передаю cook_dict как аргумент.
@@ -26,5 +26,12 @@ print(cook_dict)
 
 new_cook_dict = get_shop_list_by_dishes(cook_dict, ['Запеченный картофель', 'Омлет'], 2)
 print(new_cook_dict)
+
+
+'''
+Задание 3
+Функция лежит в папке в sorted_files
+ в файлике sorted_func
+'''
 
 get_sorted_files_file()
